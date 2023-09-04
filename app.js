c1ac4e73b6357ba39da1bb0e95c737d83b56e3e3
landry-geek <landryrakotomalala10@gmail.com>
2023-09-04 03:15:13 +0100

    config(app): routes config (#8)

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+
+const postsRouter = require('./routes/posts.router')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use('/api/personnes', postsRouter)
+
 const port = 5000
 
 app.get('/', (req, res)=>{res.send('Hello express')})
