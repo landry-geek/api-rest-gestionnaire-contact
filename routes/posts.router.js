b00328f6e0d69f1ae88d9b915bbec7d55172edb0
landry-geek <landryrakotomalala10@gmail.com>
2023-09-05 14:44:16 +0100

    create(routes): create route for update query (#16)

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -6,5 +6,6 @@ const postsController = require('../controller/posts.controller')
 router.get("/", postsController.getAll)
 router.get("/:id", postsController.getById)
 router.post("/", postsController.create)
+router.put("/:id", postsController.update)
 
 module.exports = router
