e52bf6e3a0832b4be615c6e91d26da38b62af8c9
landry-geek <landryrakotomalala10@gmail.com>
2023-09-06 08:19:58 +0100

    fix(crud-Node): fix header control allowing (#19)

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 
 const postsRouter = require('./routes/posts.router')
 
+app.use(function (req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
